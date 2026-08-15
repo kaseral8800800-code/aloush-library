e8cd606f541b9837ac3d6abebd7bc28c18c954e7
kaseral8800800-code <kaseral8800800@gmail.com>
2026-08-16 01:01:05 +0300

    Refactor service worker for caching strategy

--- sw.js
+++ sw.js
@@ -1,38 +1,4 @@
-const CACHE_NAME = 'aloush-library-v1';
-const urlsToCache = [
-    '/aloush-library/',
-    '/aloush-library/index.html',
-    '/aloush-library/manifest.json'
-];
-
-self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => cache.addAll(urlsToCache))
-            .then(() => self.skipWaiting())
-    );
-});
-
-self.addEventListener('activate', event => {
-    event.waitUntil(
-        caches.keys().then(cacheNames => {
-            return Promise.all(
-                cacheNames.map(cache => {
-                    if (cache !== CACHE_NAME) return caches.delete(cache);
-                })
-            );
-        })
-    );
-});
-
-self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request)
-            .then(response => response || fetch(event.request))
-            .catch(() => {
-                if (event.request.mode === 'navigate') {
-                    return caches.match('/aloush-library/');
-                }
-            })
-    );
-});
+const CACHE_NAME = 'aloush-v1';
+const urlsToCache = ['/aloush-library/', '/aloush-library/index.html', '/aloush-library/manifest.json'];
+self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(urlsToCache)).then(() => self.skipWaiting())); });
+self.addEventListener('fetch', e => { e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))); });
